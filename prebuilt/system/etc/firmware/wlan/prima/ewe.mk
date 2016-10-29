dd62bf76975e8b15b9fb052238c211ab0b8d9657
Taco <derpytacozlol@gmail.com>
2016-10-29 09:34:59 -0400

    Create ewe.mk

---
+++ prebuilt/system/etc/firmware/wlan/prima/ewe.mk
@@ -0,0 +1,16 @@
+ows user to override the factory
+
+# defaults for the WLAN Driver
+
+
+# Enable IMPS or not
+gEnableImps=1
+
+# Enable/Disable Idle Scan
+
+gEnableIdleScan=0
+
+
+# Increase sleep duration (seconds) during IMPS
+# 0 implies no periodic wake up from IMPS. Periodic wakeup is
+# unnecessary if Idle Scan is disabled.
